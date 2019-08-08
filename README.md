a0d6bb3cbaf54f604bfbf3d694a80aaba64faad1
Glitch (hello-express) <none>
2019-08-08 16:41:47 +0000

    🍖💋 Checkpoint ./commands/info.js:5674830/81 ./README.md:5674830/34 ./watch.json:5674830/2

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-
-Guns For Role
-=================
-
 Bot de discord for getting guns to play in role envieroment
 
 ![enter image description here](https://cdn.glitch.com/8001c8ec-f31b-4e99-a0f8-d2b02a0c3fb2%2FStory_act_037.png?v=1565277586969)
--- commands/info.js
+++ commands/info.js
@@ -1,5 +1,5 @@
 module.exports.run = async (bot, message, args) => {
-  message.channel.send('Para mas información visita : https://renziito.github.io/gunsforrole/')
+  message.channel.send('Para mas información visita : https://renziito.github.io/gunsforrole')
 }
 module.exports.help = {
   name: "info"
