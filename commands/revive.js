a63523c350736834cc26c6891e55b1fbb1adb8eb
Glitch (hello-express) <none>
2019-08-11 02:01:40 +0000

    🌯🔖 Checkpoint ./commands/revive.js:5674830/830 ./package.json:5674830/662 ./watch.json:5674830/6 ./.glitch-assets:5674830/362

--- commands/revive.js
+++ commands/revive.js
@@ -1,13 +1,15 @@
 const Discord = require('discord.js');
+const { loadImage } = require("canvas");
 
 module.exports.run = async (bot, message, args) => {
-  const mentionedUser = message.mentions.users.first() || message.author;
+  const mentionedUser = message.mentions.users.first();
   
+  if(typeof(mentionedUser)=="undefined") return message.reply("Debes mencionar a alguien");
   
   let emb = new Discord.RichEmbed()
-  .setAuthor("Tenga su "+name)
+  .setAuthor(mentionedUser.username + " ha sido reanimado")
   .setColor("RANDOM")
-  .setImage("");
+  .setImage("https://cdn.glitch.com/8001c8ec-f31b-4e99-a0f8-d2b02a0c3fb2%2F12.jpg?v=1565488589862");
 
   message.channel.send(emb)
 }
